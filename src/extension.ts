16622f2f3ee0912cd259823a0f03392459eae7f8
Ritwick Dey <ritwickdey@outlook.com>
2017-09-27 23:41:47 +0530

    folder & file detection fixed

--- src/extension.ts
+++ src/extension.ts
@@ -1,18 +1,26 @@
 'use strict';
 import * as vscode from 'vscode';
 import * as path from 'path';
+import * as fs from 'fs';
 import { AppModel } from "./appModel";
 
 export function activate(context: vscode.ExtensionContext) {
     const appModel = new AppModel();
 
     context.subscriptions.push(vscode.commands.registerCommand('extension.createFile', (file: vscode.Uri) => {
-        appModel.createFileOrFolder('file', file ? path.dirname(file.fsPath) : '/');
+        appModel.createFileOrFolder('file', file ? findDir(file.fsPath) : '/');
     }));
 
     context.subscriptions.push(vscode.commands.registerCommand('extension.createFolder', (file: vscode.Uri) => {
-        appModel.createFileOrFolder('folder', file ? path.dirname(file.fsPath) : '/');
+        appModel.createFileOrFolder('folder', file ? findDir(file.fsPath) : '/');
     }));
+}
+
+function findDir(filePath: string) {
+    if (fs.statSync(filePath).isFile())
+        return path.dirname(filePath);
+
+    return filePath;
 }
 
 export function deactivate() {
